fa8435de87ac3735dc4eba198b14bad0ead64aa9
tatsuya06068 <fhkt863@gmail.com>
2020-12-11 21:09:44 +0900

    materialize追加/taskview表示

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import '../styles/index'
 import "materialize-css/dist/js/materialize.min.js";
 M.AutoInit();
 
