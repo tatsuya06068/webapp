2d091accda86664a2d67400dad08eb71cb91bd5d
tatsuya06068 <fhkt863@gmail.com>
2020-12-13 19:54:41 +0900

    新規登録画面作成完了、一覧表示完了

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   def index
+    @tasks =Task.all
   end
 
   def show
@@ -7,5 +8,17 @@ class TasksController < ApplicationController
 
   def new
     @task = Task.new
+  end
+
+  def create
+    task = Task.new(task_params)
+    task.save!
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を登録しました。"
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :description)
   end
 end
